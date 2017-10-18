5aa70e9adadc454c803924eead9be6e2fe1cb8bd
zby007 <zhangbiyun1990@163.com>
2017-10-18 17:35:09 +0800

    alter file: calculator2.py

--- calculator2.py
+++ calculator2.py
@@ -2,60 +2,68 @@
 
 import sys
 
+def calShebao(salary):
+    sheBao = salary*(0.08 + 0.02 + 0.005 + 0.06)
+    return sheBao
+
+   
+def calTax(salary,sbMoney):
+    try:
+            
+        taxMoney = salary - sbMoney -3500
+        finalTax = 0.00
+        if (salary-sbMoney) <= 3500:
+            finalTax = 0.00          
+        elif taxMoney <=1500:
+            finalTax = taxMoney * 0.03 - 0    
+            
+        elif taxMoney <=4500: 
+            finalTax = taxMoney * 0.1 - 105        
+
+        elif taxMoney <=9000: 
+            finalTax = taxMoney * 0.2 - 555        
+
+        elif taxMoney <=35000: 
+            finalTax = taxMoney * 0.25 - 1005        
+
+        elif taxMoney <=55000: 
+            finalTax = taxMoney * 0.3 - 2755        
+
+        elif taxMoney <=80000: 
+            finalTax = taxMoney * 0.35 - 5505        
+
+        else: 
+            finalTax = taxMoney * 0.45 - 13505        
+
+    except ValueError:
+        print("Parameter Error")
+
+    finally:
+        return finalTax
+
+
 
 if __name__ == "__main__":
 
     try:
-        
-        if len(sys.argv) < 2:
+        lenInput = len(sys.argv)
+        if lenInput < 2:
             raise ValueError()
         else:
-            salDict=sys.argv[1].split(':')
-            print(salDict)
+            salList = sys.argv[1:]       
+            salDict={}
+            for tmpStr in salList:
+                keyStr = tmpStr.split(':')
+                gongHao = keyStr[0]
+                salary = int(keyStr[1])
+
+                sheBao = calShebao(salary)
+                tax = calTax(salary,sheBao)
+                finalSal = salary - sheBao -tax
+                salDict[gongHao] = finalSal
+            
+            print(len(salDict))
  
     except ValueError:
         print("Parameter Error")
 
-
-   
-'''
-try:
-    if len(sys.argv) > 2:
-        raise ValueError()
-    
-    salary=int(sys.argv[1])   
-
-    taxMoney = salary - 0 -3500
-  
-    if salary <= 3500:          
-        print(format(0,".2f"))
-    elif taxMoney <=1500:
-        finalTax = taxMoney * 0.03 - 0    
-        print(format(finalTax,".2f"))
-    elif taxMoney <=4500: 
-        finalTax = taxMoney * 0.1 - 105        
-        print(format(finalTax,".2f"))
-
-    elif taxMoney <=9000: 
-        finalTax = taxMoney * 0.2 - 555        
-        print(format(finalTax,".2f"))
-
-    elif taxMoney <=35000: 
-        finalTax = taxMoney * 0.25 - 1005        
-        print(format(finalTax,".2f"))
-
-    elif taxMoney <=55000: 
-        finalTax = taxMoney * 0.3 - 2755        
-        print(format(finalTax,".2f"))
-
-    elif taxMoney <=80000: 
-        finalTax = taxMoney * 0.35 - 5505        
-        print(format(finalTax,".2f"))
-
-    else: 
-        finalTax = taxMoney * 0.45 - 13505        
-        print(format(finalTax,".2f"))
-
-except ValueError:
-    print("Parameter Error")
-'''
