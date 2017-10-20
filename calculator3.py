805349684d8a66925ba79d29195cc72594551556
zby007 <zhangbiyun1990@163.com>
2017-10-20 09:42:15 +0800

    add 4 files:  calculator3.py test.cfg user.csv gongzi.csv

---
+++ calculator3.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+class Config(object):
+    def __init__(self,configfile):
+        self._configfile = configfile
+        self._configdict = {}
+        with open('./{0}'.format(self._configfile)) as cfile:
+            for tmpline in cfile:
+                tmpstr = tmpline.split('=')
+                self._configdict[tmpstr[0].strip()]=tmpstr[1].strip() 
+
+    def get_configdict(self):
+        print(self._configdict)     
+            
+
+
+def calShebao(salary):
+    sheBao = salary*(0.08 + 0.02 + 0.005 + 0.06)
+    return sheBao
+
+   
+def calTax(salary,sbMoney):
+    try:
+            
+        taxMoney = salary - sbMoney -3500
+        finalTax = 0.00
+        if (salary-sbMoney) <= 3500:
+            finalTax = 0.00          
+        elif taxMoney <=1500:
+            finalTax = taxMoney * 0.03 - 0    
+            
+        elif taxMoney <=4500: 
+            finalTax = taxMoney * 0.1 - 105        
+
+        elif taxMoney <=9000: 
+            finalTax = taxMoney * 0.2 - 555        
+
+        elif taxMoney <=35000: 
+            finalTax = taxMoney * 0.25 - 1005        
+
+        elif taxMoney <=55000: 
+            finalTax = taxMoney * 0.3 - 2755        
+
+        elif taxMoney <=80000: 
+            finalTax = taxMoney * 0.35 - 5505        
+
+        else: 
+            finalTax = taxMoney * 0.45 - 13505        
+
+    except ValueError:
+        print("Parameter Error")
+
+    finally:
+        return finalTax
+
+
+
+if __name__ == "__main__":
+
+
+
+    rconfig = Config('test.cfg')
+    rconfig.get_configdict()
+'''
+    try:
+        lenInput = len(sys.argv)
+        if lenInput < 2:
+            raise ValueError()
+        else:
+            salList = sys.argv[1:]       
+            salDict={}
+            for tmpStr in salList:
+                keyStr = tmpStr.split(':')
+                gongHao = int(keyStr[0])
+                salary = int(keyStr[1])
+
+                sheBao = calShebao(salary)
+                tax = calTax(salary,sheBao)
+                finalSal = salary - sheBao -tax
+                salDict[gongHao] = finalSal
+            
+            for key,value in salDict.items():
+                print("{0}:{1:.2f}".format(key,value))
+            
+           
+    except ValueError:
+        print("Parameter Error")
+'''
